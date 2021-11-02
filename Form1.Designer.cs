bc604cbeb0c1f3735b29b8b56411941c64a8d7eb
David Osborne <david.osborne@outlook.com>
2021-11-02 10:17:55 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace CharCount
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.tbCharLen = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numUpDown = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbInput
+            // 
+            this.tbInput.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbInput.Location = new System.Drawing.Point(12, 12);
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(352, 25);
+            this.tbInput.TabIndex = 0;
+            this.tbInput.TextChanged += new System.EventHandler(this.TbInput_TextChanged);
+            // 
+            // tbCharLen
+            // 
+            this.tbCharLen.Location = new System.Drawing.Point(12, 47);
+            this.tbCharLen.Name = "tbCharLen";
+            this.tbCharLen.ReadOnly = true;
+            this.tbCharLen.Size = new System.Drawing.Size(43, 20);
+            this.tbCharLen.TabIndex = 1;
+            this.tbCharLen.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "characters";
+            // 
+            // numUpDown
+            // 
+            this.numUpDown.Location = new System.Drawing.Point(141, 48);
+            this.numUpDown.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numUpDown.Name = "numUpDown";
+            this.numUpDown.Size = new System.Drawing.Size(55, 20);
+            this.numUpDown.TabIndex = 1;
+            this.numUpDown.ValueChanged += new System.EventHandler(this.NumUpDown_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(202, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "max length";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(375, 78);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numUpDown);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbCharLen);
+            this.Controls.Add(this.tbInput);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CharCount";
+            ((System.ComponentModel.ISupportInitialize)(this.numUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbInput;
+        private System.Windows.Forms.TextBox tbCharLen;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numUpDown;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CharCount
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            numUpDown.Value = 30;
+        }
+
+        public int charLen;
+
+        private void TbInput_TextChanged(object sender, EventArgs e)
+        {
+            charLen = tbInput.Text.Length;
+            MaxLengthCheck(charLen);
+        }
+
+        private void MaxLengthCheck (int charLen)
+        {
+            tbCharLen.Text = charLen.ToString();
+            if (charLen > numUpDown.Value)
+            {
+                tbCharLen.BackColor = Color.LightPink;
+            }
+            else
+                tbCharLen.BackColor = Color.LightGreen;
+        }
+
+        private void NumUpDown_ValueChanged(object sender, EventArgs e)
+        {
+            MaxLengthCheck(charLen);
+        }
+    }
+}
